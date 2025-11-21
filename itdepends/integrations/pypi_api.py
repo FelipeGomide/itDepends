f41376f014ba330617ea85ae5888ed179f099191
Felipe Gomide <flgomide@gmail.com>
2025-11-21 11:51:39 -0300

    feat: Add PyPi Api integration: get source repo from package name

--- itdepends/integrations/pypi_api.py
+++ itdepends/integrations/pypi_api.py
@@ -1,1 +1,64 @@
 import requests
+import re
+
+class PyPiClient():
+    def __init__(self, timeout=10):
+        self.session = create_session()
+        self.session.timeout = timeout
+        self.base_url = "https://pypi.org/pypi/"
+        
+    def do_safe_request(self, url):
+        try:
+            response = self.session.get(url)
+            
+            if response.status_code == 200:
+                return response.json(), None
+            
+            return None, response.status_code
+            
+        except requests.exceptions.Timeout:
+            return None, "timeout"
+        
+    def get_github_repo_name(self, package_name):
+        url = self.base_url + package_name + "/json"
+        
+        data, error = self.do_safe_request(url)
+        
+        if error:
+            return None, error
+
+        info = data.get("info", {})
+        
+        project_urls = info.get("project_urls", {})
+        
+        package_github = None
+        
+        for section in project_urls:
+            url = project_urls.get(section, None)
+            
+            if "github.com" in url:
+                package_github = url
+                break;
+
+        if not package_github:
+            return False, f"Couldn't find file with name '{package_name}'."
+        
+        match = re.search(r"github\.com/([^/]+)/([^/]+)", package_github)
+        
+        if match:
+            owner = match.group(1)
+            repo = match.group(2).split(".")[0] # Remove .git or other extensions
+            return True, owner + "/" + repo
+        else:
+            return False, f"URL found ('{package_github}'), but couldn't parse owner/repo."
+        
+def create_session():
+    session = requests.Session()
+    
+    headers = {
+        "User-Agent": "PyPiClient-itDepends",
+        "Accept": "application/json"
+    }
+
+    session.headers.update(headers)
+    return session
