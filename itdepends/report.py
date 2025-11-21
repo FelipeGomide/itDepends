9b2e5bf643cc227f23cfd4799b72a62ab30a4a63
Felipe Gomide <flgomide@gmail.com>
2025-11-21 08:52:15 -0300

    feat: Updated gitignore for results folder

---
+++ itdepends/report.py
@@ -0,0 +1,15 @@
+import plotly
+import jinja2
+
+def generate_history_plot(repository_name):
+    pass
+
+def generate_full_report():
+    
+    # pegar template na pasta
+    
+    # gerar o plot do histórico
+    
+    # 
+    
+    pass
